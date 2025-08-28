b4c9d8efde62a917218d1dd958666104338f871d
Deepachoudhary2604 <142567373+Deepachoudhary2604@users.noreply.github.com>
2025-08-28 15:30:42 +0530

    Add 1168_duplicate_zeros.java - LeetCode Problem 1168

---
+++ 1168_duplicate_zeros.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int n=arr.length-1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                for(int j=n;j>i;j--){
+                    arr[j]=arr[j-1];
+                }
+                i++;
+            }
+            
+            
+        }
+    }
+}
