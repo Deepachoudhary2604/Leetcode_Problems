50b070bb8dd257665dfbce6d770c1d16c1de2bc4
Deepachoudhary2604 <142567373+Deepachoudhary2604@users.noreply.github.com>
2025-08-28 15:43:09 +0530

    Update 88_merge_sorted_array.java - LeetCode Problem 88

--- 88_merge_sorted_array.java
+++ 88_merge_sorted_array.java
@@ -2,21 +2,14 @@ class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int i=0;
         int j=0;
-        int k=0;
-        int[] ans=new int[m];
-        while(i<m && j<n){
-            if(nums1[i]>nums2[j]){
-                ans[k]=nums2[j];
-                j++;
-                k++;
-            }
-            else{
-                ans[k]=nums1[i];
-                i++;
-                k++;
-            }
+        while(nums1[i]!=0){
+            i++;
         }
-        
-        
+        while(i<nums1.length){
+            nums1[i]=nums2[j];
+            i++;
+            j++;
+        }
+        Arrays.sort(nums1);
     }
 }
