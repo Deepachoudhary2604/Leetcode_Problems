2a6ca5cff0c37b3f950b5c2bb77043db95881301
Deepachoudhary2604 <142567373+Deepachoudhary2604@users.noreply.github.com>
2025-10-01 10:52:28 +0530

    Time: 3 ms (80.30%) | Memory: 45.5 MB (87.89%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n=ratings.length;
+        int[] arr1=new int[n];
+        int[] arr2=new int[n];
+        int sum=0;
+        arr1[0]=1;
+        arr2[n-1]=1;
+        for(int i=0;i<n-1;i++){
+            if(ratings[i+1]>ratings[i]){
+                arr1[i+1]=arr1[i]+1;
+            }
+            else{
+                arr1[i+1]=1;
+            }
+        }
+        for(int i=n-1;i>0;i--){
+            if(ratings[i]<ratings[i-1]){
+                arr2[i-1]=arr2[i]+1;
+            }
+            else{
+                arr2[i-1]=1;
+            }
+        }
+        for(int i=0;i<n;i++){
+            sum+=Math.max(arr1[i],arr2[i]);
+        }
+        return sum;
+    }
+}
