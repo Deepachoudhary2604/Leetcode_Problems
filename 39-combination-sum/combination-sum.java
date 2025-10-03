10a872efed31178b4c43118b08801e56ba6c712c
Deepachoudhary2604 <142567373+Deepachoudhary2604@users.noreply.github.com>
2025-10-04 02:06:06 +0530

    Time: 9 ms (5.47%) | Memory: 45.1 MB (8.54%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,4 +1,5 @@
 class Solution {
+    static Set<List<Integer>> set=new HashSet<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<Integer> l1=new ArrayList<>();
         List<List<Integer>> ans=new ArrayList<>();
@@ -8,16 +9,19 @@ class Solution {
     }
     public static void combination(int[] candidate,int target,List<Integer> l1,int idx,List<List<Integer>> ans){
         if(target==0){
-            ans.add(new ArrayList<>(l1));
+            if(!set.contains(l1)){
+                ans.add(new ArrayList<>(l1));
+                set.add(l1);
+            }
             return;
         }
-        for(int i=idx;i<candidate.length;i++){
-            if(target>=candidate[i]){
-                l1.add(candidate[i]);
-                combination(candidate,target-candidate[i],l1,i,ans);
-                l1.remove(l1.size()-1);
-            }
+        if(idx==candidate.length || target<0){
+            return;
         }
-    
+        l1.add(candidate[idx]);
+        combination(candidate,target-candidate[idx],l1,idx+1,ans);
+        combination(candidate,target-candidate[idx],l1,idx,ans);
+        l1.remove(l1.size()-1);
+        combination(candidate,target,l1,idx+1,ans);
     }
 }
