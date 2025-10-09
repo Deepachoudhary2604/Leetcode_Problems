f954cec135e15a4c3aa03a03b449783357269a7b
Deepachoudhary2604 <142567373+Deepachoudhary2604@users.noreply.github.com>
2025-10-10 00:03:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (47.91%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -5,7 +5,7 @@ class Solution {
         solve(nums,0,ans1,ans2);
         return ans1;
     }
-    public static void solve(int[] nums,int idx,List<List<Integer>> ans1,List<Integer> ans2){
+    public void solve(int[] nums,int idx,List<List<Integer>> ans1,List<Integer> ans2){
         if(idx==nums.length){
             ans1.add(new ArrayList<>(ans2));
             return;
