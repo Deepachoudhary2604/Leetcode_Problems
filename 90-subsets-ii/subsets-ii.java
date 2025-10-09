7862470cd408cae09ba185395c726c69360c6a2b
Deepachoudhary2604 <142567373+Deepachoudhary2604@users.noreply.github.com>
2025-10-10 00:10:17 +0530

    Time: 2 ms (28.34%) | Memory: 44.8 MB (7.35%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,26 +1,23 @@
 class Solution {
-      Set<List<Integer>> set=new HashSet<>();
+    Set<List<Integer>> set=new HashSet<>();
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>>  ans1=new ArrayList<>();
+        List<List<Integer>> ans1=new ArrayList<>();
         List<Integer> ans2=new ArrayList<>();
         solve(nums,0,ans1,ans2);
-        if(!set.contains(new ArrayList<>())){
-            ans1.add(new ArrayList<>());
-        }
         return ans1;
     }
-    public   void solve(int[] nums,int idx,List<List<Integer>> ans1,List<Integer> ans2){
+    public void solve(int[] nums,int idx,List<List<Integer>> ans1,List<Integer> ans2){
         if(idx==nums.length){
-            if(!set.contains(ans2)){
-                set.add(new ArrayList<>(ans2));
+            if(!set.contains(new ArrayList<>(ans2))){
                 ans1.add(new ArrayList<>(ans2));
+                set.add(new ArrayList<>(ans2));
             }
             return;
         }
-        solve(nums,idx+1,ans1,ans2);
         ans2.add(nums[idx]);
         solve(nums,idx+1,ans1,ans2);
         ans2.remove(ans2.size()-1);
+        solve(nums,idx+1,ans1,ans2);
     }
 }
