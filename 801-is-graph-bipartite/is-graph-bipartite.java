54cfcdca3a82183562885fdf58b53b3b6374df56
Deepachoudhary2604 <142567373+Deepachoudhary2604@users.noreply.github.com>
2025-10-31 07:24:05 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (55.15%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -4,13 +4,12 @@ class Solution {
         Arrays.fill(color,-1);
         for(int i=0;i<color.length;i++){
             if(color[i]==-1){
-                if(dfs(graph,i,color,1)==false){
+                if(dfs(graph,i,color,0)==false){
                     return false;
                 }
             }
         }
         return true;
-
     }
     public boolean dfs(int[][] graph,int currNode,int[] color,int currColor){
         color[currNode]=currColor;
@@ -19,6 +18,7 @@ class Solution {
                 if(dfs(graph,neibr,color,1-currColor)==false){
                     return false;
                 }
+                
             }
             if(color[neibr]==color[currNode]){
                 return false;
