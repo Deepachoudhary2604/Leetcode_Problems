9f1baf797a753d9e621ba65c683c3ca19c81eaad
Deepachoudhary2604 <142567373+Deepachoudhary2604@users.noreply.github.com>
2025-11-09 18:20:20 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (9.78%) - LeetSync

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -18,10 +18,8 @@ class Solution {
         if(root==null){
             return false;
         }
-        if(root!=null && (root.left==null && root.right==null)){
-            if(root.val==targetSum){
-                return true;
-            }
+        if(root.left==null && root.right==null && root.val==targetSum){
+           return true;
         }
         return hasPathSum(root.left,targetSum-root.val)||hasPathSum(root.right,targetSum-root.val);
     }
