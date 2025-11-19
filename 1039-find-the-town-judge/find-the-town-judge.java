a91e6dc58d7e12db6b32dc32feb35268793be523
Deepachoudhary2604 <142567373+Deepachoudhary2604@users.noreply.github.com>
2025-11-19 09:23:03 +0530

    Time: 3 ms (69.14%) | Memory: 53.3 MB (84.95%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -3,16 +3,16 @@ class Solution {
         if(n==1){
             return 1;
         }
-        int[] arr1=new int[n+1];
-        int[] arr2=new int[n+1];
+        int[] trustby=new int[n+1];
+        int[] truston=new int[n+1];
         for(int i=0;i<trust.length;i++){
             int a=trust[i][0];
             int b=trust[i][1];
-            arr1[a]++;
-            arr2[b]++;
+            trustby[a]++;
+            truston[b]++;
         }
-        for(int i=0;i<arr1.length;i++){
-            if(arr1[i]==0 && arr2[i]==n-1){
+        for(int i=0;i<trustby.length;i++){
+            if(truston[i]==n-1 && trustby[i]==0){
                 return i;
             }
         }
