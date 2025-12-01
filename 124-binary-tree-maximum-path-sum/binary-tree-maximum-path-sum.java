694bd04cf28194cd1608391d2491b3959d9de952
Deepachoudhary2604 <142567373+Deepachoudhary2604@users.noreply.github.com>
2025-12-01 13:30:29 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (42.36%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -25,17 +25,17 @@ class Solution {
         if (node == null) return 0;
 
         // Ignore paths with negative sums
-        int leftGain = Math.max(maxGain(node.left), 0);
-        int rightGain = Math.max(maxGain(node.right), 0);
+        int leftSum = Math.max(maxGain(node.left), 0);
+        int rightSum = Math.max(maxGain(node.right), 0);
 
         // Path sum including both children
-        int currentPathSum = node.val + leftGain + rightGain;
+        int currentPathSum = node.val + leftSum + rightSum;
 
         // Update global max if current path is better
         maxSum = Math.max(maxSum, currentPathSum);
 
         // Return max gain if we continue this path upwards
-        return node.val + Math.max(leftGain, rightGain);
+        return node.val + Math.max(leftSum, rightSum);
     
     }
 }
