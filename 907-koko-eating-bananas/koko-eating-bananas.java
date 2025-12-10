a730ad253de7bcb860183bc4c7f7cfb45dbe22ab
Deepachoudhary2604 <142567373+Deepachoudhary2604@users.noreply.github.com>
2025-12-10 21:49:24 +0530

    Time: 12 ms (64.90%) | Memory: 47.6 MB (50.68%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -8,9 +8,9 @@ class Solution {
         int ans=0;
         while(s<=e){
             int mid=s+(e-s)/2;
-            if(canEat(piles,mid,h)){
-                ans=mid;
+            if(caneat(piles,mid,h)){
                 e=mid-1;
+                ans=mid;
             }
             else{
                 s=mid+1;
@@ -18,13 +18,14 @@ class Solution {
         }
         return ans;
     }
-    public static boolean canEat(int[] piles,int mid,int h){
+    public boolean caneat(int[] piles,int mid,int h){
         long hour=0;
-        for(int p:piles){
-            hour+=p/mid;
-            if(p%mid!=0){
+        for(int pile:piles){
+            hour+=(pile/mid);
+            if(pile%mid!=0){
                 hour++;
             }
+
         }
         return hour<=h;
     }
