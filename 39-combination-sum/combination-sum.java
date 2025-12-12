762f1c246468560f4f80861520cd8fd676c57449
Deepachoudhary2604 <142567373+Deepachoudhary2604@users.noreply.github.com>
2025-12-12 06:17:10 +0530

    Time: 10 ms (5.36%) | Memory: 46.6 MB (5.62%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,27 +1,26 @@
 class Solution {
-    Set<List<Integer>> set=new HashSet<>();
+    HashSet<List<Integer>> set=new HashSet<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<Integer> l1=new ArrayList<>();
         List<List<Integer>> ans=new ArrayList<>();
-        combination(candidates,target,l1,0,ans);
+        List<Integer> l1=new ArrayList<>();
+        helper(candidates,ans,l1,target,0);
         return ans;
-        
     }
-    public void combination(int[] candidate,int target,List<Integer> l1,int idx,List<List<Integer>> ans){
+    public void helper(int[] arr,List<List<Integer>> ans,List<Integer> l1,int target,int idx){
         if(target==0){
-            if(!set.contains(l1)){
+            if(!set.contains(new ArrayList<>(l1))){
                 ans.add(new ArrayList<>(l1));
                 set.add(new ArrayList<>(l1));
             }
             return;
         }
-        if(idx==candidate.length || target<0){
+        if(idx==arr.length || target<0){
             return;
         }
-        l1.add(candidate[idx]);
-        combination(candidate,target-candidate[idx],l1,idx+1,ans);
-        combination(candidate,target-candidate[idx],l1,idx,ans);
+        l1.add(arr[idx]);
+        helper(arr,ans,l1,target-arr[idx],idx+1);
+        helper(arr,ans,l1,target-arr[idx],idx);
         l1.remove(l1.size()-1);
-        combination(candidate,target,l1,idx+1,ans);
+        helper(arr,ans,l1,target,idx+1);
     }
 }
