fed9e06570e13d8047dbebecb0039d4bd47fa813
Deepachoudhary2604 <142567373+Deepachoudhary2604@users.noreply.github.com>
2026-01-01 18:23:35 +0530

    Time: 2 ms (90.07%) | Memory: 44.6 MB (11.46%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,53 +1,50 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
+        int fresh=0;
         int row=grid.length;
         int col=grid[0].length;
-        int fresh=0;
         Queue<int[]> q=new LinkedList<>();
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==2){
+                    q.add(new int[]{i,j});
+                }
                 if(grid[i][j]==1){
                     fresh++;
-                }
-                if(grid[i][j]==2){
-                    q.offer(new int[] {i,j});
                 }
             }
         }
         if(fresh==0){
             return 0;
         }
-
         int min=0;
         int[][] dirs={{1,0},{-1,0},{0,1},{0,-1}};
-
         while(!q.isEmpty()){
-            int size=q.size();
             boolean rotten=false;
-            for(int i=0;i<size;i++){
+            int size=q.size();
+            while(size>0){
                 int[] val=q.poll();
-                int r=val[0];
-                int c=val[1];
+                int x=val[0];
+                int y=val[1];
                 for(int[] dir:dirs){
-                    int nr=r+dir[0];
-                    int nc=c+dir[1];
-                    if(nr>=0 && nc>=0 && nr<row && nc<col && grid[nr][nc]==1){
-                        grid[nr][nc]=2;
-                        q.offer(new int[]{nr,nc});
+                    int nx=x+dir[0];
+                    int ny=y+dir[1];
+                    if(nx>=0 && ny>=0 && nx<row && ny<col && grid[nx][ny]==1){
+                        q.add(new int[]{nx,ny});
                         fresh--;
+                        grid[nx][ny]=2;
                         rotten=true;
                     }
                 }
-                
+                size--;
             }
             if(rotten){
-                    min++;
+                min++;
             }
         }
-        if(fresh==0){
-            return min;
+        if(fresh!=0){
+            return -1;
         }
-        return -1;
+        return min;
     }
-
 }
