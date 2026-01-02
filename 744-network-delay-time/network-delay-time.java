45d79342a07bc1ccd6c338075cc8360e0e0bd0ed
Deepachoudhary2604 <142567373+Deepachoudhary2604@users.noreply.github.com>
2026-01-02 07:00:17 +0530

    Time: 10 ms (85.22%) | Memory: 49.2 MB (60.30%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,45 +1,45 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
         ArrayList<ArrayList<int[]>> adj=new ArrayList<>();
-        for(int i=0;i<n+1;i++){
+        for(int i=0;i<=n;i++){
             adj.add(new ArrayList<>());
         }
-        for(int[] t:times){
-            int u=t[0];
-            int v=t[1];
-            int w=t[2];
-            adj.get(u).add(new int[] {v,w});
+        for(int[] time:times){
+            int u=time[0];
+            int v=time[1];
+            int t=time[2];
+            adj.get(u).add(new int[]{v,t});
         }
-
-        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
-        pq.add(new int[]{0,k});
         int[] dist=new int[n+1];
         Arrays.fill(dist,Integer.MAX_VALUE);
         dist[k]=0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a, b) -> a[1] - b[1]
+        );
+        pq.add(new int[]{k, 0});
 
-        while(!pq.isEmpty()){
-            int[] curr=pq.poll();
-            int u=curr[1];
-            int d=curr[0];
-            if(d>dist[u]){
-                continue;
-            }
-            for(int[] neibr:adj.get(u)){
-                int node=neibr[0];
-                int w=neibr[1];
-                if(w+dist[u]<dist[node]){
-                    dist[node]=w+dist[u];
-                    pq.offer(new int[]{dist[node],node});
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int node = cur[0];
+            int time = cur[1];
+
+            if (time > dist[node]) continue;
+
+            for (int[] nei : adj.get(node)) {
+                int v = nei[0];
+                int wt = nei[1];
+
+                if (dist[node] + wt < dist[v]) {
+                    dist[v] = dist[node] + wt;
+                    pq.add(new int[]{v, dist[v]});
                 }
             }
         }
-        int max=Integer.MIN_VALUE;
-        for(int i=1;i<dist.length;i++){
-            max=Math.max(dist[i],max);
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] == Integer.MAX_VALUE) return -1;
+            ans = Math.max(ans, dist[i]);
         }
-        if(max!=Integer.MAX_VALUE){
-            return max;
-        }
-        return -1;
+        return ans;
     }
 }
