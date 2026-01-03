4effc9be9b633297ce6b2c38b917419873bb0ac1
Deepachoudhary2604 <142567373+Deepachoudhary2604@users.noreply.github.com>
2026-01-03 18:59:11 +0530

    Time: 83 ms (30.56%) | Memory: 72.9 MB (25.27%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,35 +1,24 @@
 class Solution {
-
     Boolean[][] dp;
-
     public boolean canPartition(int[] nums) {
         int sum = 0;
         for (int num : nums) sum += num;
-
         if (sum % 2 != 0) return false;
-
         int target = sum / 2;
         int n = nums.length;
-
         dp = new Boolean[n][target + 1];
-
         return knap(nums, n - 1, target);
     }
 
     private boolean knap(int[] nums, int index, int target) {
-
-        // Base cases
         if (target == 0) return true;
         if (index < 0 || target < 0) return false;
 
-        // Memo check
         if (dp[index][target] != null)
             return dp[index][target];
 
-        // Take or not take
         boolean take = knap(nums, index - 1, target - nums[index]);
         boolean notTake = knap(nums, index - 1, target);
-
         return dp[index][target] = take || notTake;
     }
 }
