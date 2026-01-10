76b29d4610c05357602300917925417cc2042c30
Deepachoudhary2604 <142567373+Deepachoudhary2604@users.noreply.github.com>
2026-01-10 14:18:07 +0530

    Time: 15 ms (47.06%) | Memory: 49.4 MB (31.90%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -2,17 +2,18 @@ class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int m=mat.length;
         int n=mat[0].length;
-        int[][] ans=new int[m][n];
+        int[][] adj=new int[m][n];
         for(int i=0;i<m;i++){
-            Arrays.fill(ans[i],-1);
+            Arrays.fill(adj[i],-1);
         }
         Queue<int[]> q=new LinkedList<>();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(mat[i][j]==0){
-                    ans[i][j]=0;
+                    adj[i][j]=0;
                     q.add(new int[]{i,j});
                 }
+                
             }
         }
         int[][] dirs={{1,0},{-1,0},{0,1},{0,-1}};
@@ -23,12 +24,12 @@ class Solution {
             for(int[] dir:dirs){
                 int nx=x+dir[0];
                 int ny=y+dir[1];
-                if(nx>=0 && ny>=0 && nx<m && ny<n && ans[nx][ny]==-1){
-                    ans[nx][ny]=ans[x][y]+1;
+                if(nx>=0 && ny>=0 && nx<m && ny<n && adj[nx][ny]==-1){
+                    adj[nx][ny]=adj[x][y]+1;
                     q.add(new int[]{nx,ny});
                 }
             }
         }
-        return ans;
+        return adj;
     }
 }
