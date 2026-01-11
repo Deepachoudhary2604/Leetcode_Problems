ec406870319610ad97cb59cfbc93aa9207d6d200
Deepachoudhary2604 <142567373+Deepachoudhary2604@users.noreply.github.com>
2026-01-11 14:42:15 +0530

    Time: 4 ms (81.67%) | Memory: 47.1 MB (33.66%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,40 +1,31 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> st = new Stack<>();
-        st.push(asteroids[0]);
-
-        for (int i = 1; i < asteroids.length; i++) {
-            int val2 = asteroids[i];
-
-            // 🔹 CHANGE 1: while loop + correct collision condition
-            while (!st.isEmpty() && st.peek() > 0 && val2 < 0) {
-                int val1 = st.peek();
-
-                // 🔹 CHANGE 2: handle sizes correctly
-                if (Math.abs(val1) < Math.abs(val2)) {
-                    st.pop();
-                }
-                else if (Math.abs(val1) == Math.abs(val2)) {
-                    st.pop();
-                    val2 = 0;   // both destroyed
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<asteroids.length;i++){
+            int val2=asteroids[i];
+            while(!st.isEmpty() && st.peek()>0 && val2<0){
+                int val1=st.peek();
+                if(Math.abs(val1)>Math.abs(val2)){
+                    val2=0;
                     break;
                 }
-                else {
-                    val2 = 0;   // current destroyed
+                if(Math.abs(val2)>Math.abs(val1)){
+                    st.pop();
+                }
+                else{
+                    st.pop();
+                    val2=0;
                     break;
                 }
             }
-
-            // 🔹 CHANGE 3: push only if alive
-            if (val2 != 0) {
+            if(val2!=0){
                 st.push(val2);
             }
         }
-
-        int[] adj = new int[st.size()];
-        for (int i = st.size() - 1; i >= 0; i--) {
-            adj[i] = st.pop();
+        int[] ans=new int[st.size()];
+        for(int i=st.size()-1;i>=0;i--){
+            ans[i]=st.pop();
         }
-        return adj;
+        return ans;
     }
 }
