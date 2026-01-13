cb21b4d7efa9b9a29096dc58d737db637fc3b233
Deepachoudhary2604 <142567373+Deepachoudhary2604@users.noreply.github.com>
2026-01-13 11:50:42 +0530

    Time: 518 ms (14.22%) | Memory: 62 MB (5.23%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -4,9 +4,6 @@ class Solution {
         List<List<Integer>> ans=new ArrayList<>();
         Set<List<Integer>> set=new HashSet<>();
         for(int i=0;i<nums.length-2;i++){
-            if(i>0 && nums[i]==nums[i-1]){
-                continue;
-            }
             int j=i+1;
             int k=nums.length-1;
             while(j<k){
@@ -24,13 +21,9 @@ class Solution {
                     adj.add(nums[k]);
                     j++;
                     k--;
-                   
-                    ans.add(new ArrayList<>(adj));
-                    while(j<k && nums[j]==nums[j-1]){
-                        j++;
-                    }
-                    while(j<k && nums[k]==nums[k+1]){
-                        k--;
+                    if(!set.contains(new ArrayList<>(adj))){
+                        set.add(new ArrayList<>(adj));
+                        ans.add(new ArrayList<>(adj));
                     }
                     
                 }
