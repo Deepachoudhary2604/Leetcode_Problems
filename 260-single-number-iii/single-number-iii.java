4351992133c015bd0fe2cdf156b982a27d62ceeb
Deepachoudhary2604 <142567373+Deepachoudhary2604@users.noreply.github.com>
2026-01-15 11:43:56 +0530

    Time: 1 ms (99.77%) | Memory: 48 MB (67.80%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -1,20 +1,24 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        HashSet<Integer> set=new HashSet<>();
-        for(int num:nums){
-            if(!set.contains(num)){
-                set.add(num);
-            }
-            else{
-                set.remove(num);
+        int ans1 = 0;
+        int ans2 = 0;
+        int xor = 0;
+
+        for (int num : nums) {
+            xor ^= num;
+        }
+
+        // FIX
+        int maskBit = xor & -xor;
+
+        for (int num : nums) {
+            if ((num & maskBit) == 0) {
+                ans1 ^= num;
+            } else {
+                ans2 ^= num;
             }
         }
-        int j=0;
-        int[] ans=new int[set.size()];
-        for(int i:set){
-            ans[j]=i;
-            j++;
-        }
-        return ans;
+
+        return new int[]{ans1, ans2};
     }
 }
