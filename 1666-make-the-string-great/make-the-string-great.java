2d4e2bcb6e9a5d948dc11e9ce36e1f73e12f8955
Deepachoudhary2604 <142567373+Deepachoudhary2604@users.noreply.github.com>
2026-01-23 12:41:22 +0530

    Time: 3 ms (64.70%) | Memory: 43.4 MB (77.49%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.java
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -5,15 +5,9 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
 
-            if (!st.isEmpty()) {
-                if (ch >= 'A' && ch <= 'Z' && Character.toLowerCase(ch) == st.peek()) {
-                    st.pop();
-                    continue;
-                }
-                if (ch >= 'a' && ch <= 'z' && Character.toUpperCase(ch) == st.peek()) {
-                    st.pop();
-                    continue;
-                }
+            if(!st.isEmpty() && (int)(Math.abs(ch-st.peek()))==32){
+                st.pop();
+                continue;
             }
             st.push(ch);
         }
