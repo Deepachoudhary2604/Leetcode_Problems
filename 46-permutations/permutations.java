a2fd3a75e8c5acf572666c4a6bb3bd90d55af617
Deepa Choudhary <142567373+Deepachoudhary2604@users.noreply.github.com>
2026-02-24 21:44:39 +0530

    Time: 2 ms (37.64%) | Memory: 45.8 MB (29.36%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,27 +1,24 @@
 class Solution {
+    HashSet<Integer> set=new HashSet<>();
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans1=new ArrayList<>();
-        permutation(nums,ans1,0);
-        return ans1;
+        List<List<Integer>> l1=new ArrayList<>();
+        List<Integer> l2=new ArrayList<>();
+        helper(nums,l1,l2);
+        return l1;
     }
-    public void permutation(int[] nums,List<List<Integer>> ans1,int idx){
-        if(idx==nums.length){
-            List<Integer> l1=new ArrayList<>();
-            for(int i=0;i<nums.length;i++){
-                l1.add(nums[i]);
-            }
-            ans1.add(l1);
+    public void helper(int[] nums,List<List<Integer>> l1,List<Integer> l2){
+        if(l2.size()==nums.length){
+            l1.add(new ArrayList<>(l2));
             return;
         }
-        for(int i=idx;i<nums.length;i++){
-            swap(nums,i,idx);
-            permutation(nums,ans1,idx+1);
-            swap(nums,i,idx);
+        for(int i=0;i<nums.length;i++){
+            if(!set.contains(nums[i])){
+                set.add(nums[i]);
+                l2.add(nums[i]);
+                helper(nums,l1,l2);
+                set.remove(nums[i]);
+                l2.remove(l2.size()-1);
+            }
         }
-    }
-    public static void swap(int[] nums,int i,int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
     }
 }
