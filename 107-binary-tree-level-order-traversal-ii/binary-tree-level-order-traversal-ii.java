40d8518fb0bba5c0fba597080c08123392a12b51
Deepa Choudhary <142567373+Deepachoudhary2604@users.noreply.github.com>
2026-03-07 10:33:30 +0530

    Time: 1 ms (96.91%) | Memory: 44.5 MB (56.80%) - LeetSync

---
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ans=new ArrayList<>();
+        helper(root,ans);
+        return rev(ans);
+        
+    }
+    public void helper(TreeNode root,List<List<Integer>> ans){
+        if (root == null) return ;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            int size = q.size();                 
+            ArrayList<Integer> l1 = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                l1.add(node.val);
+                if (node.left != null) q.add(node.left);
+                if (node.right != null) q.add(node.right);
+            }
+            ans.add(l1);                      
+        }
+    }
+    public List<List<Integer>> rev(List<List<Integer>> ans){
+        List<List<Integer>> r=new ArrayList<>();
+        for(int i=ans.size()-1;i>=0;i--){
+            List<Integer> l=ans.get(i);
+            r.add(new ArrayList<>(l));
+
+        }
+        return r;
+    }
+}
