267bec082b71085e3842acd3d2db8069d040e9ef
Deepa Choudhary <142567373+Deepachoudhary2604@users.noreply.github.com>
2026-03-09 16:01:17 +0530

    Time: 1785 ms (5.03%) | Memory: 47.8 MB (20.08%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,25 +1,23 @@
 class Solution {
     public List<Integer> findAnagrams(String s2, String s1) {
-        List<Integer> ans=new ArrayList<>();
-        if (s1.length() > s2.length()) return ans;
-        int[] arr1=new int[26];
-        for(int i=0;i<s1.length();i++){
-            arr1[s1.charAt(i)-'a']++;
-        }
-        int[] arr2=new int[26];
+        List<Integer> a=new ArrayList<>();
+        if (s1.length() > s2.length()) return a;
         int i=0;
-        int j=0;
-        while(j<s2.length()){
-            arr2[s2.charAt(j)-'a']++;
-            if(j-i+1>s1.length()){
-                arr2[s2.charAt(i)-'a']--;
-                i++;
+        int j=s1.length()-1;
+        char[] arr=s1.toCharArray();
+        Arrays.sort(arr);
+        String s=new String(arr);
+        while(j<=s2.length()-1){
+            String ans=s2.substring(i,j+1);
+            char[] ans1=ans.toCharArray();
+            Arrays.sort(ans1);
+            String ans2=new String(ans1);
+            if(s.equals(ans2)){
+                a.add(i);
             }
-            if(j-i+1==s1.length() && Arrays.equals(arr1,arr2)){
-                ans.add(i);
-            }
+            i++;
             j++;
         }
-        return ans;
+        return a;
     }
 }
