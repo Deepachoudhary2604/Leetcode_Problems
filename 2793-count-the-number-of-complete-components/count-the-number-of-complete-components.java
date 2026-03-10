6a9ca4335ce2cb52a47ed3bc55274748af1518f0
Deepa Choudhary <142567373+Deepachoudhary2604@users.noreply.github.com>
2026-03-10 10:58:54 +0530

    Time: 8 ms (89.89%) | Memory: 47.7 MB (54.73%) - LeetSync

---
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -0,0 +1,44 @@
+class Solution {
+    int nodes=0;
+    int ed=0;
+    public int countCompleteComponents(int n, int[][] edges) {
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+            
+        }
+        for(int[] edge:edges){
+            int u=edge[0];
+            int v=edge[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        int c=0;
+        boolean[] vis=new boolean[n];
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                nodes = 0;
+                ed = 0;
+
+                dfs(adj, vis, i);
+
+                ed= ed/ 2;
+
+                if (ed == nodes * (nodes - 1) / 2) {
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+    public void dfs(ArrayList<ArrayList<Integer>> adj,boolean[] vis,int s){
+        vis[s]=true;
+        nodes++;
+        ed += adj.get(s).size();
+        for(int neibr:adj.get(s)){
+            if(!vis[neibr]){
+                dfs(adj,vis,neibr);
+            }
+        }
+    }
+}
