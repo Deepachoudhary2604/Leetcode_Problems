8f91e2d2b70a0d90ba2bec103ad7abdb8bed7390
Deepa Choudhary <142567373+Deepachoudhary2604@users.noreply.github.com>
2026-03-13 11:07:50 +0530

    Time: 2 ms (90.73%) | Memory: 44.3 MB (53.02%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,50 +1,47 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
         int fresh=0;
-        int row=grid.length;
-        int col=grid[0].length;
         Queue<int[]> q=new LinkedList<>();
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==2){
-                    q.add(new int[]{i,j});
-                }
                 if(grid[i][j]==1){
                     fresh++;
+                }
+                if(grid[i][j]==2){
+                    q.add(new int[]{i,j});
                 }
             }
         }
         if(fresh==0){
             return 0;
         }
-        int min=0;
+        int time=0;
         int[][] dirs={{1,0},{-1,0},{0,1},{0,-1}};
         while(!q.isEmpty()){
             boolean rotten=false;
             int size=q.size();
-            while(size>0){
+            for(int i=0;i<size;i++){
                 int[] val=q.poll();
-                int x=val[0];
-                int y=val[1];
+                int r=val[0];
+                int c=val[1];
                 for(int[] dir:dirs){
-                    int nx=x+dir[0];
-                    int ny=y+dir[1];
-                    if(nx>=0 && ny>=0 && nx<row && ny<col && grid[nx][ny]==1){
-                        q.add(new int[]{nx,ny});
+                    int nr=r+dir[0];
+                    int nc=c+dir[1];
+                    if(nr>=0 && nc>=0 && nr<grid.length && nc<grid[0].length && grid[nr][nc]==1){
+                        q.add(new int[]{nr,nc});
                         fresh--;
-                        grid[nx][ny]=2;
                         rotten=true;
+                        grid[nr][nc]=2;
                     }
                 }
-                size--;
             }
             if(rotten){
-                min++;
+                time++;
             }
         }
         if(fresh!=0){
             return -1;
         }
-        return min;
+        return time;
     }
 }
