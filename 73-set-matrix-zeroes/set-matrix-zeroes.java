f818ed8428053556c903de6daba7acbedade310d
Deepa Choudhary <142567373+Deepachoudhary2604@users.noreply.github.com>
2026-06-04 14:39:27 +0530

    Time: 39 ms (5.36%) | Memory: 48.1 MB (5.81%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,9 +1,9 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        List<Integer> row=new ArrayList<>();
-        List<Integer> col=new ArrayList<>();
+        ArrayList<Integer> row=new ArrayList<>();
+        ArrayList<Integer> col=new ArrayList<>();
         for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
+            for(int j=0;j<matrix[i].length;j++){
                 if(matrix[i][j]==0){
                     row.add(i);
                     col.add(j);
@@ -11,15 +11,13 @@ class Solution {
             }
         }
         for(int i=0;i<row.size();i++){
-            int r=row.get(i);
-            for(int j=0;j<matrix[0].length;j++){
-                matrix[r][j]=0;
+            for(int j=0;j<matrix[row.get(i)].length;j++){
+                matrix[row.get(i)][j]=0;
             }
         }
         for(int i=0;i<col.size();i++){
-            int c=col.get(i);
             for(int j=0;j<matrix.length;j++){
-                matrix[j][c]=0;
+                matrix[j][col.get(i)]=0;
             }
         }
     }
