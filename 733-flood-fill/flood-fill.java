16ce80d3f5039303784b90d50b279b6781821827
Deepa Choudhary <142567373+Deepachoudhary2604@users.noreply.github.com>
2026-06-18 19:31:38 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (25.08%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,21 +1,20 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int curr=image[sr][sc];
-        if(curr==color){
+        int temp=image[sr][sc];
+        if(temp==color){
             return image;
         }
-        helper(image,sr,sc,color,curr);
+        dfs(image,sr,sc,color,temp);
         return image;
     }
-    public void helper(int[][] image,int sr,int sc,int color,int curr){
-        if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length || image[sr][sc]!=curr){
+    public void dfs(int[][] image, int sr, int sc, int color,int temp){
+        if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length || image[sr][sc]!=temp){
             return;
         }
         image[sr][sc]=color;
-        helper(image,sr+1,sc,color,curr);
-        helper(image,sr-1,sc,color,curr);
-        helper(image,sr,sc-1,color,curr);
-        helper(image,sr,sc+1,color,curr);
-
+        dfs(image,sr+1,sc,color,temp);
+        dfs(image,sr-1,sc,color,temp);
+        dfs(image,sr,sc+1,color,temp);
+        dfs(image,sr,sc-1,color,temp);
     }
 }
