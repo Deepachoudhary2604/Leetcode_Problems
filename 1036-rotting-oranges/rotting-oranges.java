ea0f74b8b89eced32a8681a0a7d6dcacc1cb8f58
Deepa Choudhary <142567373+Deepachoudhary2604@users.noreply.github.com>
2026-06-20 22:21:56 +0530

    Time: 2 ms (90.91%) | Memory: 44.1 MB (71.67%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -3,11 +3,11 @@ class Solution {
         int fresh=0;
         Queue<int[]> q=new LinkedList<>();
         for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
+            for(int j=0;j<grid[i].length;j++){
                 if(grid[i][j]==1){
                     fresh++;
                 }
-                if(grid[i][j]==2){
+                else if(grid[i][j]==2){
                     q.add(new int[]{i,j});
                 }
             }
@@ -15,33 +15,33 @@ class Solution {
         if(fresh==0){
             return 0;
         }
-        int time=0;
+        int min=0;
         int[][] dirs={{1,0},{-1,0},{0,1},{0,-1}};
         while(!q.isEmpty()){
-            boolean rotten=false;
             int size=q.size();
-            for(int i=0;i<size;i++){
-                int[] val=q.poll();
-                int r=val[0];
-                int c=val[1];
+            boolean rotten=false;
+            while(size>0){
+                int[] temp=q.poll();
+                int r=temp[0];
+                int c=temp[1];
                 for(int[] dir:dirs){
                     int nr=r+dir[0];
                     int nc=c+dir[1];
                     if(nr>=0 && nc>=0 && nr<grid.length && nc<grid[0].length && grid[nr][nc]==1){
-                        q.add(new int[]{nr,nc});
                         fresh--;
-                        rotten=true;
                         grid[nr][nc]=2;
+                        q.add(new int[]{nr,nc});
+                        rotten=true;
                     }
+
                 }
+                size--;
             }
             if(rotten){
-                time++;
+                min++;
             }
         }
-        if(fresh!=0){
-            return -1;
-        }
-        return time;
+        if(fresh==0) return min;
+        return -1;
     }
 }
