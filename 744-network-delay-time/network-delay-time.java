ca91952252a332a343f5b608d11d1c25b2edbdef
Deepa Choudhary <142567373+Deepachoudhary2604@users.noreply.github.com>
2026-06-21 11:04:03 +0530

    Time: 10 ms (83.84%) | Memory: 48.9 MB (61.51%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,45 +1,43 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
+        int[] dist=new int[n+1];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[k]=0;
+
         ArrayList<ArrayList<int[]>> adj=new ArrayList<>();
         for(int i=0;i<=n;i++){
             adj.add(new ArrayList<>());
         }
-        for(int[] time:times){
-            int u=time[0];
-            int v=time[1];
-            int t=time[2];
-            adj.get(u).add(new int[]{v,t});
+        for(int i=0;i<times.length;i++){
+            int u=times[i][0];
+            int v=times[i][1];
+            int w=times[i][2];
+            adj.get(u).add(new int[]{v,w});
         }
-        int[] dist=new int[n+1];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-        dist[k]=0;
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a, b) -> a[1] - b[1]
-        );
-        pq.add(new int[]{k, 0});
-
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int node = cur[0];
-            int time = cur[1];
-
-            if (time > dist[node]) continue;
-            dist[node]=time;
-
-            for (int[] nei : adj.get(node)) {
-                int v = nei[0];
-                int wt = nei[1];
-
-                if (dist[node] + wt < dist[v]) {
-                    dist[v] = dist[node] + wt;
-                    pq.add(new int[]{v, dist[v]});
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[1]-b[1]);
+        pq.add(new int[]{k,0});
+        while(!pq.isEmpty()){
+            int[] temp=pq.poll();
+            int node=temp[0];
+            int d=temp[1];
+            if(d>dist[node]){
+                continue;
+            }
+            for(int[] neibr:adj.get(node)){
+                int nextNode=neibr[0];
+                int nextWt=neibr[1];
+                if(dist[nextNode]>nextWt+dist[node]){
+                    dist[nextNode]=nextWt+dist[node];
+                    pq.add(new int[]{nextNode,dist[nextNode]});
                 }
             }
         }
-        int ans = 0;
-        for (int i = 1; i <= n; i++) {
-            if (dist[i] == Integer.MAX_VALUE) return -1;
-            ans = Math.max(ans, dist[i]);
+        int ans=0;
+        for(int i=1;i<dist.length;i++){
+            ans=Math.max(ans,dist[i]);
+        }
+        if(ans==Integer.MAX_VALUE){
+            return -1;
         }
         return ans;
     }
