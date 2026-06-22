5a35a39980a21e7e42c440618bb9b8365c7c9bf8
Deepa Choudhary <142567373+Deepachoudhary2604@users.noreply.github.com>
2026-06-22 09:41:23 +0530

    Time: 9 ms (99.64%) | Memory: 75.4 MB (19.56%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,36 +1,31 @@
 class Solution {
     Boolean[][] dp;
-    public boolean canPartition(int[] arr) {
-        int n=arr.length;
-        int s=0;
-        for(int i=0;i<arr.length;i++){
-            s+=arr[i];
+    public boolean canPartition(int[] nums) {
+        int total=0;
+        int n=nums.length;
+        for(int i:nums){
+            total+=i;
         }
-        if(s%2!=0){
+        if(total%2!=0){
             return false;
         }
-        int sum=s/2;
-        dp = new Boolean[n+1][sum+1]; 
-        return solve(arr,sum,n);
+        int sum=total/2;
+        dp=new Boolean[n+1][sum+1];
+        return part(nums,n,sum);
     }
-    public Boolean solve(int[] arr,int sum,int n){
+    public boolean part(int[] nums,int n,int sum){
+        if(sum==0){
+            return true;
+        }
         if(n==0){
-            if(sum==0){
-                return true;
-            }
             return false;
         }
-       
         if(dp[n][sum]!=null){
             return dp[n][sum];
         }
-        boolean pick=false;
-        if(sum>=arr[n-1]){
-            pick=solve(arr,sum-arr[n-1],n-1) || solve(arr,sum,n-1);
+        if(nums[n-1]>sum){
+            return dp[n][sum]=part(nums,n-1,sum);
         }
-        boolean not=solve(arr,sum,n-1);
-        return dp[n][sum]=pick || not;
-        
+        return dp[n][sum]=part(nums,n-1,sum-nums[n-1]) || part(nums,n-1,sum);
     }
-    
 }
