1d389f2892e434a1a219fd02e27de85439199ec6
Deepa Choudhary <142567373+Deepachoudhary2604@users.noreply.github.com>
2026-07-13 19:22:55 +0530

    Time: 1 ms (94.20%) | Memory: 43.6 MB (15.76%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -3,21 +3,18 @@ class Solution {
         List<List<Integer>> ans=new ArrayList<>();
         int row=0;
         int star=1;
-        
         while(row<numRows){
-            int i=0;
             int val=1;
-            List<Integer> arr=new ArrayList<>();
+            int i=0;
+            List<Integer> temp=new ArrayList<>();
             while(i<star){
-                arr.add(val);
-                val=(row-i)*val/(i+1);
+                temp.add(val);
+                val=((row-i)*val)/(i+1);
                 i++;
             }
-            ans.add(arr);
-            row++;
             star++;
-
-
+            row++;
+            ans.add(temp);
         }
         return ans;
     }
